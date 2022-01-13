cc1822eb6fe7c69d6fd218fb41994fefb6d8001d
Le Hoang Bao Chung <1851061805@e.tlu.edu.vn>
2022-01-13 23:37:16 +0700

    little changes

---
+++ ApplicationAlgorithm/src/py/levenshtein.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def levenshtein(seq1, seq2):
+    size_x = len(seq1) + 1
+    size_y = len(seq2) + 1
+    matrix = np.zeros((size_x, size_y))
+    for x in range(size_x):
+        matrix[x, 0] = x
+    for y in range(size_y):
+        matrix[0, y] = y
+
+    for x in range(1, size_x):
+        for y in range(1, size_y):
+            if seq1[x - 1] == seq2[y - 1]:
+                matrix[x, y] = min(matrix[x - 1, y] + 1, matrix[x - 1, y - 1], matrix[x, y - 1] + 1)
+            else:
+                matrix[x, y] = min(matrix[x - 1, y] + 1, matrix[x - 1, y - 1] + 1, matrix[x, y - 1] + 1)
+    return (matrix[size_x - 1, size_y - 1])
+
+a = input("Nhap A = ")
+b = input("Nhap B = ")
+print("So thao tac it nhat de bien doi A thanh B =", int(levenshtein(a, b)))
